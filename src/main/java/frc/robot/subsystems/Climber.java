cd2bef48cf27af2d35995d5f86635fff0617ba1f
WINDOWS-NBCUQSR\Liberty Robotics <chad.lee@live.com>
2024-01-11 20:04:49 -0600

    Created the Climber subsystem class and added bits

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,14 +4,44 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
+import frc.robot.libraries.internal.LazyTalonFX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class Climber extends SubsystemBase {
   /** Creates a new Climber. */
 
-  public Climber() {
+  public LazyTalonFX climberMotor1;
+  public LazyTalonFX climberMotor2;
+  public PIDController pidController;
+  public DigitalInput rightLimitSwitch;
+  public DigitalInput leftLimitSwitch;
+  
 
+  public Climber() {
+    climberMotor1 = new LazyTalonFX(Constants.CLIMBER_MOTOR1.id, Constants.CLIMBER_MOTOR1.busName);
+    climberMotor2 = new LazyTalonFX(Constants.CLIMBER_MOTOR2.id, Constants.CLIMBER_MOTOR2.busName);
+    //double desiredEncoder;
+  
+   
+    //double variable = pidController.calculate(getEncoderValue(),desiredEncoderValue);
+    
+  }
+  
+  public void climberOn(double climberSpeed) {
+    if(!rightLimitSwitch.get() || !leftLimitSwitch.get()){
+      double newClimberSpeed = climberSpeed < 0 ? 0 : climberSpeed;
+      climberMotor1.set(ControlMode.PercentOutput, newClimberSpeed);
+      climberMotor2.set(ControlMode.PercentOutput, newClimberSpeed);
+    }
+  }
+
+  public void climberOff() {
+    climberMotor1.set(ControlMode.PercentOutput, 0);
+    climberMotor2.set(ControlMode.PercentOutput, 0);
   }
 
   @Override
