0514920f90c288c496db96b65294d7c2eff6f9c5
Team1764 <First1764@gmail.com>
2024-02-03 16:27:35 -0600

    constants

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -8,16 +8,16 @@ public final class Constants {
     public static final int PRESSURE_SENSOR_PORT =   0;
 
 
-    public static final CanPort CLIMBER_MOTOR1 =     new CanPort(21);
-    public static final CanPort CLIMBER_MOTOR2 =     new CanPort(21);
+    public static final CanPort CLIMBER_MOTOR1 =     new CanPort(10); //right
+    public static final CanPort CLIMBER_MOTOR2 =     new CanPort(20); //left
 
-    public static final CanPort FLYWHEEL_MOTOR1 =     new CanPort(21);
-    public static final CanPort FLYWHEEL_MOTOR2 =     new CanPort(21);
-    public static final CanPort HOLDER_MOTOR =     new CanPort(21);
+    public static final CanPort FLYWHEEL_MOTOR1 =     new CanPort(8); //right
+    public static final CanPort FLYWHEEL_MOTOR2 =     new CanPort(36); //left
+    public static final CanPort HOLDER_MOTOR =     new CanPort(33); //index, climber, roller
 
     public static final CanPort INTAKE_MOTOR =     new CanPort(21);
-    public static final CanPort WRIST_MOTOR1 =     new CanPort(21);
-    public static final CanPort WRIST_MOTOR2 =     new CanPort(21);
+    public static final CanPort WRIST_MOTOR1 =     new CanPort(35);
+    public static final CanPort WRIST_MOTOR2 =     new CanPort(26);
 
 
     public static final int HOLDER_BREAK_BEAM = 1;
